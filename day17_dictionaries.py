1de0664a71915d1ba4a70c1eb8f34f43d01eaaf3
AncyDataScience <selva2711992@gmail.com>
2026-07-28 13:18:45 +0530

    Create day17_dictionaries.py

---
+++ day17_dictionaries.py
@@ -0,0 +1,17 @@
+# Day 17: List of Dictionaries for Sports Analytics
+
+players_data = [
+    {"name": "Cristiano Ronaldo", "goals": 30, "team": "Al Nassr"},
+    {"name": "Erling Haaland", "goals": 25, "team": "Manchester City"},
+    {"name": "Jude Bellingham", "goals": 22, "team": "Real Madrid"}
+]
+
+# Accessing and printing data for each player
+print(players_data[0]["name"], "scored", players_data[0]["goals"], "goals for", players_data[0]["team"])
+print(players_data[1]["name"], "scored", players_data[1]["goals"], "goals for", players_data[1]["team"])
+print(players_data[2]["name"], "scored", players_data[2]["goals"], "goals for", players_data[2]["team"])
+
+# OUTPUT 
+Cristiano Ronaldo scored 30 goals for Al Nassr
+Erling Haaland scored 25 goals for Manchester City
+Jude Bellingham scored 22 goals for Real Madrid
