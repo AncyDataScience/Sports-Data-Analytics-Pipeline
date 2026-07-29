7d9a05ae9f3731eb138b3c7bcbb79a235c92e76f
AncyDataScience <selva2711992@gmail.com>
2026-07-29 14:52:42 +0530

    Update day18_loops.py

--- day18_loops.py
+++ day18_loops.py
@@ -14,3 +14,22 @@ for player in players_data:
 Cristiano Ronaldo scored 30 goals for Al Nassr
 Erling Haaland scored 25 goals for Manchester City
 Jude Bellingham scored 22 goals for Real Madrid
+
+# Day 18: Filtering top scorers using For Loop and If Condition
+
+players_data = [
+    {"name": "Cristiano Ronaldo", "goals": 30, "team": "Al Nassr"},
+    {"name": "Erling Haaland", "goals": 25, "team": "Manchester City"},
+    {"name": "Jude Bellingham", "goals": 22, "team": "Real Madrid"}
+]
+
+print("## Top Scorers (25+ Goals) ##")
+
+for player in players_data:
+    if player["goals"] >= 25:
+        print(player["name"], "scored", player["goals"], "goals!")
+
+# OUTPUT
+## Top Scorers (25+ Goals) ##
+Cristiano Ronaldo scored 30 goals!
+Erling Haaland scored 25 goals!
