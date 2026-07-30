69203ea623a2a83b85dc305707154bca5cf1a117
AncyDataScience <selva2711992@gmail.com>
2026-07-30 21:37:52 +0530

    Create day19_functions.py

---
+++ day19_functions.py
@@ -0,0 +1,15 @@
+# Day 19: Introduction to Functions in Sports Analytics
+
+# Defining a function to display player summary
+def show_player_summary(name, goals, team):
+    print(name, "playing for", team, "has scored", goals, "goals this season.")
+
+# Calling the function for different players
+show_player_summary("Cristiano Ronaldo", 30, "Al Nassr")
+show_player_summary("Erling Haaland", 25, "Manchester City")
+show_player_summary("Jude Bellingham", 22, "Real Madrid")
+
+# OUTPUT
+Cristiano Ronaldo playing for Al Nassr has scored 30 goals this season.
+Erling Haaland playing for Manchester City has scored 25 goals this season.
+Jude Bellingham playing for Real Madrid has scored 22 goals this season.
